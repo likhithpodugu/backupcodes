ddcf6bc0b4386a9d332b81013a64993be50c4cee
LIKHITH PODUGU <likhithpodugu03@gmail.com>
2022-08-18 00:32:49 +0530

    Update MergeSort.c

--- datastructures/Sorting.c/MergeSort.c
+++ datastructures/Sorting.c/MergeSort.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
 
-int merge(int a[10],int lb,int mid,int ub){
-
-    
+void merge(int a[10],int lb,int mid,int ub){
+    int i = lb ;
+    int j = mid + 1 ;
+    int k = lb ;
+    int  b[10] ;
+    while(i<=mid && j<=ub){
+        if(a[i]<=a[j]){
+            b[k] = a[i] ;
+            i++ ;
+            k++ ; 
+        }
+        else{
+            b[k] = a[j] ;
+            j++ ;
+            k++ ;
+        }
+    }
+    if(i>mid){
+        while(j<=ub){
+            b[k] = a[j] ;
+            j++ ;
+            k++ ;
+        }
+    }
+    else{
+        while(i<=mid){
+            b[k] = a[i] ;
+            i++ ;
+            k++ ;
+        }
+    }
+    for(int s = lb;s<=ub;s++){
+        a[s] = b[s] ;
+    }
 }
 
-
-
-int mergesort(int a[10],int lb,int ub){
+void mergesort(int a[10],int lb,int ub){
     if(lb<ub){
-    int mid = (lb + ub)/2 ;
-    mergesort(a, lb,mid) ;
-    mergesort(a,mid+1,ub) ;
-    merge(a,lb,mid,ub) ; 
+        int mid = (lb+ub)/2;
+        mergesort(a,lb,mid) ;
+        mergesort(a,mid+1,ub) ; 
+        merge(a,lb,mid,ub) ;
+        }
+}
+
+int main(){
+    int a[10] = {19,13,22,55,66,11,53,12,38,77} ;
+    mergesort(a,0,9) ;
+    
+    for(int i = 0;i<10;i++){
+        printf("%d\t",a[i]) ;
     }
 }
