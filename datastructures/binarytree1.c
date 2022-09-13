83a01c17d2dccd05387d9087f4492141aa5aaf4e
LIKHITH PODUGU <likhithpodugu03@gmail.com>
2022-09-13 22:02:58 +0530

    Create binarytree1.c

---
+++ datastructures/binarytree1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node* var ;
+struct node{
+    int data;
+    var left ;
+    var right ;
+} ;
+
+var create(){
+    var newnode ;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter your data(type -1 for no node) : \n") ;
+    int temp ;
+    scanf("%d",&temp ) ;
+    if(temp == -1){
+        return 0 ;
+    }
+    else{
+    newnode->data = temp ;
+    }
+    printf("Enter the data left child node %d",temp);
+    newnode->left = create() ;
+    printf("Enter the data right child node %d",temp);
+    newnode->right = create() ;
+    
+    return newnode ;
+}
+
+void preorder(struct node *root){
+    if(root == 0){
+        return;
+    }
+    else{
+        printf("%d",root->data) ;
+        preorder(root->left);
+        preorder(root->right) ;
+    }
+}
+
+void inorder(struct node *root){
+    if(root == 0){
+        return ;
+    }
+    else{
+        inorder(root->left);
+        printf("%d",root->data) ;
+        inorder(root->right) ;
+    }
+}
+
+void postorder(struct node *root){
+    if(root == 0){
+        return ;
+    }
+    else{
+        postorder(root->left) ;
+        postorder(root->right) ;
+        printf("%d",root->data) ;
+    }
+}
+
+
+int main(){
+    var root ;
+    root = create() ;
+    printf("Preorder of binary tree");
+    preorder(root) ;
+    printf("Inorder of binary tree");
+    inorder(root) ;
+    printf("Postorder of binary tree");
+    postorder(root) ;
+}
