9dfa510b4aefad4567d7aae8cbe518b93a738f55
LIKHITH PODUGU <likhithpodugu03@gmail.com>
2023-03-30 23:12:43 +0530

    Update 2linesegments.c

--- datastructures/2linesegments.c
+++ datastructures/2linesegments.c
@@ -26,10 +26,35 @@ int orientation(struct point p,struct point q,struct point r){
         return -3 ;
     }
 }
+
+int doIntersect(struct point p,struct point q,struct point r ,struct point s){
+    int o1 = orientation(p,q,r) ;
+    int o2 = orientation(p,q,s) ;
+    int o3 = orientation(r,s,p) ;
+    int o4 = orientation(r,s,q) ;
+    
+    if(o1!=o2 && o3!=o4){
+        return 11 ;
+    }
+    else if(o1==0 && OnSegment(p,r,q)){
+        return 11 ;
+    }
+    else if(o2==0 && OnSegment(p,s,q)){
+        return 11 ;
+    }
+    else if(o3==0 && OnSegment(r,p,s)){
+        return 11 ;
+    }
+    else if(o4==0 && OnSegment(r,q,s)){
+        return 11 ;
+    }
+    else{
+        return -11 ;
+    }
+}
 int main(){
-    struct point p1 = {0, 1};
-    struct point p2 = {2, 2};
-    struct point p3 = {3 , 3} ;
-    printf("%d\n",OnSegment(p1,p2,p3)) ;
-    printf("%d",orientation(p1,p2,p3)) ;
+    struct point p1 = {10, 0}, q1 = {0, 10};
+    struct point p2 = {0, 0}, q2 = {10, 10};
+    printf("%d",doIntersect(p1,q1,p2,q2)) ;
+    
 }
