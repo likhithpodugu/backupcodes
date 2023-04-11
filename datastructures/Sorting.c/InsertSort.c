b09bf5b9a9e69aa8d243b9f0301b3c4a193588d3
LIKHITH PODUGU <likhithpodugu03@gmail.com>
2023-04-12 02:13:32 +0530

    Update InsertSort.c

--- datastructures/Sorting.c/InsertSort.c
+++ datastructures/Sorting.c/InsertSort.c
@@ -7,9 +7,11 @@ int main(){
     int temp ,j ;
     for(int i = 1; i<5;i++){
         temp = a[i];
-        for( j = i -1; j>=0 && a[j] > temp; j--){
+        for( j = i -1; j>=0 ; j--){
+            if(a[j] > temp){
             a[j+1] = a[j] ;
              a[j] = temp ;
+            }
         }
     }
     
